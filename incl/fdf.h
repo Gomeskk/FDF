f5b3c1a068f765f8ad4b99379222ce65cea9a846
Gomeskk <gomesjoao08@hotmail.com>
2024-10-28 12:08:30 +0000

    colors and keybind

--- incl/fdf.h
+++ incl/fdf.h
@@ -6,11 +6,18 @@
 /*   By: joafaust <joafaust@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 17:14:36 by joafaust          #+#    #+#             */
-/*   Updated: 2024/09/20 17:16:35 by joafaust         ###   ########.fr       */
+/*   Updated: 2024/10/28 12:06:12 by joafaust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
+
+# include <fcntl.h>
+#include "../minilibx-linux/mlx.h"
+# include "../LIBFT/libft.h"
+# include "colors.h"
+# include "keycode_linux.h"
+# include <stdbool.h>
 
 #endif
