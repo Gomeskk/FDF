5736c07ed8b68c1504542d1390457376328043c9
Gomeskk <gomesjoao08@hotmail.com>
2024-10-28 15:56:21 +0000

    updates

--- srcs/draw.c
+++ srcs/draw.c
@@ -6,7 +6,22 @@
 /*   By: joafaust <joafaust@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 17:20:31 by joafaust          #+#    #+#             */
-/*   Updated: 2024/10/28 12:21:05 by joafaust         ###   ########.fr       */
+/*   Updated: 2024/10/28 14:17:15 by joafaust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../incl/fdf.h"
+#include <math.h>
+
+/*
+** creates a new image
+*/
+
+void		ft_create_image(t_image *img)
+{
+	img->image ? mlx_destroy_image(img->mlx, img->image) : 0;
+	img->w ? mlx_clear_window(img->mlx, img->w) : 0;
+	img->image = mlx_new_image(img->mlx, WIDTH, HEIGHT);
+	img->data_address = mlx_get_data_addr(img->image, &(img->bpp),
+			&(img->line_size), &(img->endian));
+}
