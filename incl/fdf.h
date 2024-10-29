995f8c2d759c203fad2ae4f42b8579a5f45f5a9a
Gomeskk <gomesjoao08@hotmail.com>
2024-10-29 00:49:22 +0000

    need to correct init.c

--- incl/fdf.h
+++ incl/fdf.h
@@ -6,77 +6,153 @@
 /*   By: joafaust <joafaust@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 17:14:36 by joafaust          #+#    #+#             */
-/*   Updated: 2024/10/28 12:58:54 by joafaust         ###   ########.fr       */
+/*   Updated: 2024/10/29 00:35:55 by joafaust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-# include "../LIBFT/libft.h"
-# include "../minilibx-linux/mlx.h"
-# include "colors.h"
-# include "keycode_linux.h"
-# include <fcntl.h>
-# include <stdbool.h>
+# define HEIGHT 1080
+# define WIDTH 1920
+# define MENU_WIDTH 250
 
-# define TITLE "FDF"
-# define PI 3.14159265359
-# define HELP_COLOR WHITE
-# define HEIGHT 1400
-# define WIDTH 2000
-# define FRAME_WIDTH 200
-# define MAX_UNIT_SIZE 20
+# define FT_UINT_MAX 4294967295U // Assuming unsigned int is 32 bits
+# define FT_INT_MAX 2147483647   // Maximum positive value for a signed int
+# define FT_INT_MIN -2147483648  // Minimum value for a signed int
 
-/*
-** macros used for still inputs
-*/
-
-# define KEYPRESS 2
-# define KEYRELEASE 3
-/* # define KEYPRESSMASK (1L << 0) tenho depois de tirar isto de comment
-# define KEYRELEASEMASK (1L << 1) */
-
-/*
-** more awesome colors
-*/
-
-# define PINK 0xcc00cc
-# define BLACK 0x000000
-# define MARINE_BLUE 0x000099
-# define CRIMSON_RED 0x990033
-# define NICE_BLUE 0x6666ff
-# define DARK_TEAL 0x33cccc
-# define GREY 0xd3d3d3
-# define BROWN 0x996633
-# define TRANSPARENT 0x77000000
-
-/*
-** a is alpha(transparency) r = red g = green b = blue
-** argb stores inside .x the color used to draw the pixel
-** and in y the required incrementation
-*/
-
-typedef struct	s_argb
+typedef enum s_bool
 {
-	float		a;
-	float		r;
-	float		g;
-	float		b;
-}				t_argb;
+	false,
+	true
+}						t_bool;
 
-typedef struct	s_argb2
+typedef enum s_projection
 {
-	t_argb		x;
-	t_argb		y;
-}				t_argb2;
+	ISO,
+	PARALLEL
+}						t_projection;
 
-/*
-** color.c ~ gradient colors functions
-*/
-t_argb2			gradient(unsigned a, unsigned b, int pixel);
-t_argb			ft_hex2argb(long hex);
-long			ft_argb2hex(t_argb rgb);
-unsigned		custom_color(t_3d *d, t_argb color, int shade);
+typedef struct s_order_val
+{
+	int					z;
+	int					color;
+	struct s_order_val	*next;
+}						t_order_val;
+
+typedef struct s_point
+{
+	int					x;
+	int					y;
+	int					z;
+	int					color;
+}						t_point;
+
+typedef struct s_camera
+{
+	t_projection		projection;
+	int					zoom;
+	double				alpha;
+	double				beta;
+	double				gamma;
+	float				z_div;
+	int					x_offset;
+	int					y_offset;
+}						t_camera;
+
+typedef struct s_map
+{
+	int					width;
+	int					height;
+	int					*order_arr;
+	int					*colors_arr;
+	int					z_min;
+	int					z_max;
+	int					z_range;
+}						t_map;
+
+typedef struct s_mouse
+{
+	char				is_pressed;
+	int					x;
+	int					y;
+	int					previous_x;
+	int					previous_y;
+}						t_mouse;
+
+typedef struct s_fdf
+{
+	void				*mlx;
+	void				*win;
+	void				*img;
+	char				*data_addr;
+	int					bits_p_pix;
+	int					size_line;
+	int					endian;
+	t_camera			*cam;
+	t_map				*map;
+	t_mouse				*mouse;
+}						t_fdf;
+int						read_map(const int fd, t_order_val **orders_stack,
+							t_map *map);
+
+void					push(t_order_val **orders_stack, t_order_val *new);
+
+t_order_val				*pop(t_order_val **orders_stack);
+
+t_map					*map_init(void);
+
+t_fdf					*fdf_init(t_map *map);
+
+t_camera				*camera_init(t_fdf *fdf);
+
+void					stack_to_arrays(t_order_val **orders_stack, t_map *map);
+
+void					draw(t_map *map, t_fdf *fdf);
+
+int						get_def_color(int z, t_map *map);
+
+int						get_li(int start, int end, double percentage);
+
+int						get_color(t_point current, t_point start, t_point end,
+							t_point delta);
+
+t_point					project(t_point p, t_fdf *fdf);
+
+void					print_menu(t_fdf *fdf);
+
+int						close(void *param);
+
+void					setup_controls(t_fdf *fdf);
+
+int						key_press(int key, void *param);
+
+int						mouse_press(int button, int x, int y, void *param);
+
+int						mouse_release(int button, int x, int y, void *param);
+
+int						mouse_move(int x, int y, void *param);
+
+void					zoom(int key, t_fdf *fdf);
+
+void					move(int key, t_fdf *fdf);
+
+void					rotate(int key, t_fdf *fdf);
+
+void					compress(int key, t_fdf *fdf);
+
+void					change_projection(int key, t_fdf *fdf);
+
+t_bool					ft_isnumber(char *str, int base);
+
+int						ft_atoi_base(const char *str, int base);
+
+double					percent(int start, int end, int current);
+
+void					terminate(char *s);
+
+int						get_index(int x, int y, int width);
+
+t_point					n_point(int x, int y, t_map *map);
 
 #endif
--- srcs/controls.c
+++ srcs/controls.c
@@ -1,12 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   hooks.c                                            :+:      :+:    :+:   */
+/*   controls.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: joafaust <joafaust@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 17:20:20 by joafaust          #+#    #+#             */
-/*   Updated: 2024/10/28 12:21:53 by joafaust         ###   ########.fr       */
+/*   Updated: 2024/10/28 23:26:48 by joafaust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "fdf.h"
+#include "mlx.h"
+#include <stdlib.h>
+
+/*
+** Close window with 'X'
+*/
+
+int	close(void *param)
+{
+	(void)param;
+	exit(0);
+}
+
+/*
+** Connect functions-controllers to window
+*/
+
+void	set_controls(t_fdf *fdf)
+{
+	mlx_hook(fdf->win, 2, 0, key_press, fdf);
+	mlx_hook(fdf->win, 17, 0, close, fdf);
+	mlx_hook(fdf->win, 4, 0, mouse_press, fdf);
+	mlx_hook(fdf->win, 5, 0, mouse_release, fdf);
+	mlx_hook(fdf->win, 6, 0, mouse_move, fdf);
+}
