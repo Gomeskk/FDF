9d5019909d56da2372b0f917a7009f9cb2b7a476
Gomeskk <gomesjoao08@hotmail.com>
2024-11-04 15:13:54 +0000

    key_controls

--- srcs/key_controls.c
+++ srcs/key_controls.c
@@ -6,7 +6,37 @@
 /*   By: joafaust <joafaust@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 17:20:28 by joafaust          #+#    #+#             */
-/*   Updated: 2024/10/28 22:50:42 by joafaust         ###   ########.fr       */
+/*   Updated: 2024/11/04 15:12:27 by joafaust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "fdf.h"
+#include "keycode_linux.h"
+#include <stdlib.h>
+
+int	key_press(int key, void *param)
+{
+	t_fdf	*fdf;
+
+	fdf = (t_fdf *)param;
+	if (key == MAIN_PAD_ESC)
+		exit(0);
+	if (key == NUM_PAD_PLUS || key == MAIN_PAD_PLUS || key == NUM_PAD_MINUS
+		|| key == MAIN_PAD_MINUS)
+		zoom(key, fdf);
+	else if (key == ARROW_LEFT || key == ARROW_RIGHT || key == ARROW_UP
+		|| key == ARROW_DOWN)
+		move(key, fdf);
+	else if (key == NUM_PAD_1 || key == MAIN_PAD_1 || key == NUM_PAD_2
+		|| key == MAIN_PAD_2 || key == NUM_PAD_3 || key == MAIN_PAD_3
+		|| key == NUM_PAD_4 || key == MAIN_PAD_4 || key == NUM_PAD_6
+		|| key == MAIN_PAD_6 || key == NUM_PAD_7 || key == MAIN_PAD_7
+		|| key == NUM_PAD_8 || key == MAIN_PAD_8 || key == NUM_PAD_9
+		|| key == MAIN_PAD_9)
+		rotate(key, fdf);
+	else if (key == MAIN_PAD_LESS || key == MAIN_PAD_MORE)
+		flatten(key, fdf);
+	else if (key == MAIN_PAD_P || key == MAIN_PAD_I)
+		change_projection(key, fdf);
+	return (0);
+}
