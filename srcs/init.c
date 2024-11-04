ac4bf46311a2e07cc8a622a076b724f90aa47387
Gomeskk <gomesjoao08@hotmail.com>
2024-11-04 14:29:11 +0000

    without errors of norminette

--- srcs/init.c
+++ srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: joafaust <joafaust@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 22:51:15 by joafaust          #+#    #+#             */
-/*   Updated: 2024/10/29 00:57:21 by joafaust         ###   ########.fr       */
+/*   Updated: 2024/11/04 14:25:57 by joafaust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ t_map	*map_init(void)
 {
 	t_map	*map;
 
-	if (!(map = (t_map *)ft_memalloc(sizeof(t_map))))
+	map = (t_map *)ft_memalloc(sizeof(t_map));
+	if (!(map))
 		terminate(ERR_MAP_INIT);
 	map->width = 0;
 	map->height = 0;
@@ -43,18 +44,23 @@ t_fdf	*fdf_init(t_map *map)
 {
 	t_fdf	*fdf;
 
-	if (!(fdf = (t_fdf *)ft_memalloc(sizeof(t_fdf))))
+	fdf = (t_fdf *)ft_memalloc(sizeof(t_fdf));
+	fdf->mlx = mlx_init();
+	fdf->win = mlx_new_window(fdf->mlx, WIDTH, HEIGHT, "FdF");
+	fdf->img = mlx_new_image(fdf->mlx, WIDTH, HEIGHT);
+	fdf->mouse = (t_mouse *)ft_memalloc(sizeof(t_mouse));
+	if (!(fdf))
 		terminate(ERR_FDF_INIT);
-	if (!(fdf->mlx = mlx_init()))
+	if (!(fdf->mlx))
 		terminate(ERR_FDF_INIT);
-	if (!(fdf->win = mlx_new_window(fdf->mlx, WIDTH, HEIGHT, "FdF")))
+	if (!(fdf->win))
 		terminate(ERR_FDF_INIT);
-	if (!(fdf->img = mlx_new_image(fdf->mlx, WIDTH, HEIGHT)))
+	if (!(fdf->img))
 		terminate(ERR_FDF_INIT);
 	fdf->data_addr = mlx_get_data_addr(fdf->img, &(fdf->bits_p_pix),
 			&(fdf->size_line), &(fdf->endian));
 	fdf->map = map;
-	if (!(fdf->mouse = (t_mouse *)ft_memalloc(sizeof(t_mouse))))
+	if (!(fdf->mouse))
 		terminate(ERR_FDF_INIT);
 	return (fdf);
 }
@@ -67,7 +73,8 @@ t_camera	*camera_init(t_fdf *fdf)
 {
 	t_camera	*camera;
 
-	if (!(camera = (t_camera *)ft_memalloc(sizeof(t_camera))))
+	camera = (t_camera *)ft_memalloc(sizeof(t_camera));
+	if (!(camera))
 		terminate(ERR_CAMERA_INIT);
 	camera->zoom = FT_MIN((WIDTH - MENU_WIDTH) / fdf->map->width / 2, HEIGHT
 			/ fdf->map->height / 2);
@@ -80,5 +87,5 @@ t_camera	*camera_init(t_fdf *fdf)
 	return (camera);
 }
 
-//FT_MIN
+// FT_MIN
 // norminette convert.c draw.c
