124ae55441ea1f9de8255cfcbc7e12dfc07e2c1c
Gomeskk <gomesj778@gmail.com>
2024-11-13 21:53:29 +0000

    need to put mouse working

--- srcs/controls.c
+++ srcs/controls.c
@@ -6,7 +6,7 @@
 /*   By: joafaust <joafaust@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 17:20:20 by joafaust          #+#    #+#             */
-/*   Updated: 2024/11/04 16:25:04 by joafaust         ###   ########.fr       */
+/*   Updated: 2024/11/13 15:34:52 by joafaust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	ft_close(void *param)
 
 void	set_controls(t_fdf *fdf)
 {
-	mlx_hook(fdf->win, 2, 0, key_press, fdf);
+	mlx_hook(fdf->win, 2, (1L << 0), key_press, fdf);
 	mlx_hook(fdf->win, 17, 0, ft_close, fdf);
 	mlx_hook(fdf->win, 4, 0, mouse_press, fdf);
 	mlx_hook(fdf->win, 5, 0, mouse_release, fdf);
