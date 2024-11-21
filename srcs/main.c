db9f5bb21fc1ef49bc9de72be464f6bec8351c3a
Gomeskk <gomesjoao08@hotmail.com>
2024-11-21 15:39:31 +0000

    done

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: joafaust <joafaust@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 16:30:38 by joafaust          #+#    #+#             */
-/*   Updated: 2024/11/20 21:52:36 by joafaust         ###   ########.fr       */
+/*   Updated: 2024/11/21 15:38:56 by joafaust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,28 +21,26 @@ int	main(int argc, char **argv)
 {
 	int			fd;
 	t_map		*map;
-	t_order_val	*orders_stack;
 	t_fdf		*fdf;
+	t_order_val	*orders_stack;
 
+	if (argc != 2)
+		terminate(ERR_USAGE, NULL);
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+		terminate(ERR_MAP, NULL);
+	map = map_init();
 	orders_stack = NULL;
-	if (argc == 2)
+	if (read_map(fd, &orders_stack, map) == -1)
 	{
-		fd = open(argv[1], O_RDONLY);
-		if (!(fd))
-			terminate(ERR_MAP, NULL);
-		map = map_init();
-		if (read_map(fd, &orders_stack, map) == -1)
-		{
-			free_pop(&orders_stack);
-			terminate(ERR_MAP_READING, fdf_init(map));
-		}
-		fdf = fdf_init(map);
-		stack_to_arrays(&orders_stack, map);
-		fdf->cam = camera_init(fdf);
-		draw(fdf->map, fdf);
-		set_controls(fdf);
-		mlx_loop(fdf->mlx);
+		free_pop(&orders_stack);
+		terminate(ERR_MAP_READING, fdf_init(map));
 	}
-	terminate(ERR_USAGE, NULL);
+	fdf = fdf_init(map);
+	stack_to_arrays(&orders_stack, map);
+	fdf->cam = camera_init(fdf);
+	draw(fdf->map, fdf);
+	set_controls(fdf);
+	mlx_loop(fdf->mlx);
 	return (0);
 }
